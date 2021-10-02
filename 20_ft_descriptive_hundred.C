eeb350b5b02ae5987bfc042332204d806329c0db
DimaSoroko <84499772+DimaSoroko@users.noreply.github.com>
2021-10-02 12:05:06 +0200

    Add files via upload

---
+++ 20_ft_descriptive_hundred.C
@@ -0,0 +1,50 @@
+/*
+Write the function ft_descriptive_hundred that will take an integer as an argument
+
+The integer passed as an argument will belong to a range [0, 999]
+
+The function must print the integer following the format:
+
+n_hundred Hundred under_hundred
+The function must only call printf once with the following arguments:
+
+// Note: You are free to name variables as you wish
+printf("%d Hundred %d\n", n_hundred, under_hundred);
+If the integer argument is outside range [0, 999], nothing needs to be printed
+
+The function must be prototyped as follows: void ft_descriptive_hundred(int n)
+*/
+
+#include <stdio.h>
+
+void ft_descriptive_hundred(int n)
+{
+    int n_hundred;
+    int under_hundred;
+
+    if (n < 999)
+    {
+        if (n < 100)
+        {
+            n_hundred = 0;
+            under_hundred = n;
+        }
+        else if (n >= 100)
+        {
+            n_hundred = n / 100;
+            under_hundred = n % 100;
+        }
+
+        printf("%d Hundred %d\n", n_hundred, under_hundred);
+    }
+}
+
+/*
+int main()
+{
+    ft_descriptive_hundred(5);
+    ft_descriptive_hundred(256);
+    ft_descriptive_hundred(1000);
+    return (0);
+}
+*/
