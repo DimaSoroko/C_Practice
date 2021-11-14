18e7ad0a622bb53ae8f028f3e0fd1700630eb880
DimaSoroko <84499772+DimaSoroko@users.noreply.github.com>
2021-11-14 17:29:54 +0100

    Add files via upload

--- 40_ft_strcpy.C
+++ 40_ft_strcpy.C
@@ -14,24 +14,22 @@ char *ft_strcpy(char *dest, char *src)
     i = 0;
     while (src[i] != '\0') // reads whole src string
     {
-        dest[i] = src[i]; 
+        dest[i] = src[i];
         i++;
     }
     dest[i] = src[i]; // put src value into dest
-    return(dest);
+    return (dest);
 }
 
 
-/*
 
 int main()
 {
     char  src [] = "Test";
     char  dest [] = "";
-    printf("The value of dest is : %s\n",dest);
+    printf("The value of [dest] is : %s\n",dest);
     ft_strcpy(dest, src);
-    printf("The value of dest after strcpy is : %s\n",dest);
+    printf("The value of [dest] after [strcpy] is : %s\n",dest);
     return(0);
 }
 
-*/
