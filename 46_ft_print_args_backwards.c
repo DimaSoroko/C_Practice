e19419951bb8a6cfdecd4b60677926c946499109
DimaSoroko <84499772+DimaSoroko@users.noreply.github.com>
2021-11-18 12:55:09 +0100

    Add files via upload

--- 46_ft_print_args_backwards.c
+++ 46_ft_print_args_backwards.c
@@ -19,8 +19,24 @@ Allowed functions:	write
 */
 
 #include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+void ft_putstr(char *str)
+{
+    int i;
+    i = 0;
+    while (str[i] != '\0')
+    {
+        ft_putchar(str[i]);
+        i++;
+    }
+}
+
+int main(int argc, char **argv)
 
 {
     int i;
@@ -29,14 +45,16 @@ int main(int argc, char *argv[])
     {
         i++;
     }
-    if(argv[i] == '\0')
+    if (argv[i] == '\0')
     {
         i--;
     }
     while (i != 0)
     {
-        printf("%s ", argv[i]);
+        ft_putstr(argv[i]);
+        write(1, " ", 1);
         i--;
     }
+    
     return 0;
 }
